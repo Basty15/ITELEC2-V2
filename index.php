13452343d3f37f9e277797cb283f1423003fffb3
Basty15 <2022312181@dhvsu.edu.ph>
2024-09-30 14:33:32 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,31 +1,26 @@
 <?php
-include_once 'config/settings-configuration.php';
-?>
+require_once 'authentication/admin-class.php';
 
+$admin = new ADMIN();
+if(!$admin->isUserLoggedin())
+{
+  $admin->redirect('../../');
+}
+
+$stmt = $admin->runQuerry("SELECT * FROM user WHERE id = :id");
+$stmt->execute(array(":id"=> $_SESSION['adminSession'] ));
+$user_data = $stmt->fetch(PDO::FETCH_ASSOC);
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="stylesheet" href="src/style.css">
+    <title>ADMIN DASHBOARD</title>
 </head>
 <body>
-    <h1>SIGN IN</h1>
-    <form action="dashboard/admin/authentication/admin-class.php" method="POST">
-        <input type="hidden" name="csrf_token" value="<?php echo $csrf_token?>">
-        <input type="email" name="email" placeholder="Enter email" required> <br>
-        <input type="password" name="password" placeholder="Enter password" required> <br>
-        <button type="submit" name="btn-signin">SIGN IN</button>
-    </form>
-   
-   
-    <h1>REGISTRAION</h1>
-    <form action="dashboard/admin/authentication/admin-class.php" method="POST">
-        <input type="hidden" name="csrf_token" value="<?php echo $csrf_token?>">
-        <input type="text" name="username" placeholder="Enter username" required> <br>
-        <input type="email" name="email" placeholder="Enter email" required> <br>
-        <input type="password" name="password" placeholder="Enter password" required> <br>
-        <button type="submit" name="btn-signup">SIGN UP</button>
-    </form>
+    <h1>WELCOME <?php   echo $user_data['email']?></h1>
+    <button><a href="authentication/admin-class.php?admin_signout">SIGN OUT</a></button>
 </body>
 </html>
